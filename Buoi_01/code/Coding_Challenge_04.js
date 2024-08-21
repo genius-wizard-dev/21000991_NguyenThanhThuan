b7030846cd60e70ba127b4ef02a89040cd7d0896
iuh21000991 <21000991.thuan@student.iuh.edu.vn>
2024-08-21 08:44:28 +0700

    Bai_04

--- Buoi_01/code/Coding_Challenge_04.js
+++ Buoi_01/code/Coding_Challenge_04.js
@@ -1,12 +1,10 @@
 
-// Tip calculator
 const calculateTip = (bill) => {
   const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
   console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
   return tip;
 };
 
-// Test data
 calculateTip(275);
 calculateTip(40);
 calculateTip(430);
