80f66696b9db9c3633382e34fc2543e179192439
iuh21000991 <21000991.thuan@student.iuh.edu.vn>
2024-08-21 08:44:04 +0700

    Bai_02_Part_02

--- Buoi_01/code/Coding_Challenge_02_Part_02.js
+++ Buoi_01/code/Coding_Challenge_02_Part_02.js
@@ -1,4 +1,4 @@
-const calcTip = function(bill) {
+export const calcTip = function(bill) {
     return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
 };
 
@@ -8,6 +8,6 @@ const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
 
 const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
 
-console.log(`Bills: ${bills}`);
-console.log(`Tips: ${tips}`);
-console.log(`Total: ${total}`);
+// console.log(`Bills: ${bills}`);
+// console.log(`Tips: ${tips}`);
+// console.log(`Total: ${total}`);
