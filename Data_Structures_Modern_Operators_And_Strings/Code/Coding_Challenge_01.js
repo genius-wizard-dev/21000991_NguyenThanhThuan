b1ad08639fc0c18a0aa14e38ef3ef3e934c9907f
iuh21000991 <21000991.thuan@student.iuh.edu.vn>
2024-08-27 12:59:03 +0700

    Upload code and image Challenge 01

---
+++ Data_Structures_Modern_Operators_And_Strings/Code/Coding_Challenge_01.js
@@ -0,0 +1,58 @@
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+        [
+            'Neuer',
+            'Pavard',
+            'Martinez',
+            'Alaba',
+            'Davies',
+            'Kimmich',
+            'Goretzka',
+            'Coman',
+            'Muller',
+            'Gnarby',
+            'Lewandowski',
+        ],
+        [
+            'Burki',
+            'Schulz',
+            'Hummels',
+            'Akanji',
+            'Hakimi',
+            'Weigl',
+            'Witsel',
+            'Hazard',
+            'Brandt',
+            'Sancho',
+            'Gotze',
+        ],
+    ],
+    score: '4:0',
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+    date: 'Nov 9th, 2037',
+    odds: {
+        team1: 1.33,
+        x: 3.25,
+        team2: 6.5,
+        },
+    };
+
+const [players1, players2] = game.players;
+const [gk, ...fieldPlayers] = players1;
+const allPlayers = [...players1, ...players2];
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+
+const {team1, draw, team2} = game.odds;
+const printGoals = function(...players) {
+    console.log(`${players.length} goals were scored`);
+    players.forEach(player => console.log(player));
+};
+
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals(...game.scored);
+
+console.log("\nĐội có khả năng thắng cao hơn:");
+console.log(team1 < team2 ? game.team1 : game.team2);
+
