b5871057f9852254fa9295f54a6022b59aa10b65
iuh21000991 <21000991.thuan@student.iuh.edu.vn>
2024-08-28 08:09:15 +0700

    Upload code and image Coding_Challenge_01

--- Working_With_Arrays/Code/Coding_Challenge_01.js
+++ Working_With_Arrays/Code/Coding_Challenge_01.js
@@ -1,7 +1,8 @@
 // Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
 const juliaData = [3, 5, 2, 12, 7];
 const kateData = [4, 1, 15, 8, 3];
-
+// const juliaData_02 = [9, 16, 6, 8, 3];
+// const kateData_02 = [10, 5, 6, 1, 4];
 
 const checkDogs = function(dogsJulia, dogsKate) {
     const dogsJuliaCorrected = dogsJulia.slice();    
